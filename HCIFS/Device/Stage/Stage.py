93bb7537328066fc763bbe6e354cc263551e93c4
Matthew Grossman <mjg5@princeton.edu>
2018-07-24 15:10:43 -0400

    Updates to stages

--- HCIFS/Device/Stage/Stage.py
+++ HCIFS/Device/Stage/Stage.py
@@ -10,59 +10,22 @@ class Stage(Device):
         self.handle = None
         self.vel = None
         self.accel = None
+        self.position = None
         
-    def connect(self):
-        """
-        connects the camera and gets current position, velocity, and acceleration
-        """
-        self.pos = self.position()
-        self.vel = self.get_vel()
-        self.accel = self.get_accel()
-    def position(self):
+    def getPos(self):
         """
         returns the current position of the stage
         """
-        return 0
-    
-    def goto(self, position):
+        return self.position
+
+    def moveDevice(self, movement):
         """
-        moves the stage to position
+        moves the stage relative to its current position
         """
-        self.pos = self.position()
+        self.position += movement
     
     def gotoHome(self):
         """
         moves the stage to its home position
         """
-        self.pos = self.position()
-        
-    def get_vel(self):
-        """
-        gets the current max velocity of the stage
-        """
-        return 0
-    
-    def set_vel(self, velocity):
-        """
-        sets the max velocity of the stage to velocity
-        """
-        self.vel = self.get_vel()
-    
-    def get_accel(self):
-        """
-        gets the current acceleration of the stage
-        """
-        return 0
-        
-    def set_accel(self, acceleration):
-        """
-        sets the accleration of the stage to accleration
-        """
-        self.accel = self.get_accel()
-    
-    def cleanup(self):
-        """
-        releases the connection to the stage
-        """
-        pass
-        
+        self.position = 0
